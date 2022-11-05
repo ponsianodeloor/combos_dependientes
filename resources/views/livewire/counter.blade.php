23175e1b5d19c75b00cd5458958813f1c6bec784
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-11-05 12:08:35 -0500

    instalacion de livewire y test

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>
+    {{-- Be like water. --}}
+    <h1>Hola Livewire</h1>
+
+    <div style="text-align: center">
+        <button wire:click="increment()">+</button>
+        <h1>{{ $count }}</h1>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ciudades', function (){
+    return view('ciudades');
+});
